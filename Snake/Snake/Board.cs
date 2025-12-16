800ad3e0459d20bc3f921673fd5f629e9b7b6b9d
Tim <tim.hug@digitecgalaxus.ch>
2025-12-16 13:55:59 +0100

    fixed version ig?

--- Snake/Snake/Board.cs
+++ Snake/Snake/Board.cs
@@ -3,42 +3,43 @@ namespace Snake;
 public class Board
 {
     public bool[,] PlayingField { get; private set; }
-    
-   public bool[,] PlayingFieldGenerator(int height, int width, List<(int horizontal, int vertical)> snake, (int horizontal, int vertical) snakeHead, (int horizontal, int vertical) apple)
-   {
-       var playingField = new bool[height, width];
-       playingField = BorderMaker(height, width, playingField);
-       PlayingField = PlayingFieldUpdater(snakeHead.vertical, snakeHead.horizontal, apple.vertical, apple.horizontal, playingField,
-           snake);
-       return PlayingField;
-   }
-   
-   public bool[,] PlayingFieldUpdater(int snakeHeadVertical, int snakeHeadHorizontal, int appleVertical, int appleHorizontal, bool[,] playingField, List<(int horizontal, int vertical)> snake)
-   {
-       foreach (var part in snake)
-       {
-           playingField[part.vertical, part.horizontal] = true;
-       }
-       playingField[snakeHeadVertical, snakeHeadHorizontal] = true;
 
-       playingField[appleVertical, appleHorizontal] = true;
+    public void PlayingFieldGenerator(int height, int width, List<(int horizontal, int vertical)> snake,
+        (int horizontal, int vertical) snakeHead, (int horizontal, int vertical) apple)
+    {
+        var playingField = new bool[height, width];
 
-       PlayingField = playingField;
-       return playingField;
-   }
+        BorderMaker(height, width, playingField);
 
-   internal bool[,] BorderMaker(int height, int width, bool[,] playingField)
-   {
-       for (var row = 0; row < height; row++)
-       {
-           for (var column = 0; column < width; column++)
-           {
-               if ((row == 0 || row == height - 1 || column == 0 || column == width - 1))
-               {
-                   playingField[row, column] = true;
-               }
-           }
-       }
-       return playingField;
-   }
+        PlayingField = PlayingFieldUpdater(snakeHead.vertical, snakeHead.horizontal, apple.vertical, apple.horizontal, playingField, snake);
+    }
+
+    public static bool[,] PlayingFieldUpdater(int snakeHeadVertical, int snakeHeadHorizontal, int appleVertical, int appleHorizontal, bool[,] playingField, List<(int horizontal, int vertical)> snake)
+    {
+        foreach (var part in snake)
+        {
+            playingField[part.vertical, part.horizontal] = true;
+        }
+
+        playingField[snakeHeadVertical, snakeHeadHorizontal] = true;
+        playingField[appleVertical, appleHorizontal] = true;
+
+        return playingField;
+    }
+
+    internal bool[,] BorderMaker(int height, int width, bool[,] playingField)
+    {
+        for (var row = 0; row < height; row++)
+        {
+            for (var column = 0; column < width; column++)
+            {
+                if (row == 0 || row == height - 1 || column == 0 || column == width - 1)
+                {
+                    playingField[row, column] = true;
+                }
+            }
+        }
+
+        return playingField;
+    }
 }
